568253f85a8db7e7752201647a2dcaea6c04f59e
Sujan Ghosh <103595490+sujanrupu@users.noreply.github.com>
2022-07-09 22:36:07 +0530

    Added

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Amar gane bhor belate",
+    artist: "Shreya Ghoshal",
+    img: "music-8",
+    src: "music-4"
+  },
+  {
+    name: "E tumi kemon tumi",
+    artist: "Rupankar Bagchi",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Mangala deep jele",
+    artist: "Lata Mangeshkar",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Jodi Bondho Hao",
+    artist: "Shuvomita Banerjee",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Megher palok",
+    artist: "Shreya Ghoshal",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Kabhi jo badal barse",
+    artist: "Arijit Singh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Thodi der",
+    artist: "Farhan Saeed & Shreya Ghoshal",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
